17c7fb0f43db62fa52cc406f48975a7889aa9e5b
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2022-02-08 13:35:16 +0900

    updated a root command

--- cmd/root.go
+++ cmd/root.go
@@ -1,40 +1,93 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
 	"github.com/dilmnqvovpnmlib/gngc/gngc"
+	"github.com/dilmnqvovpnmlib/gngc/model"
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var cfgFile string
+var config model.Config
 
 var isNotified bool
 
 var rootCmd = &cobra.Command{
-	Use:   "gngc",
-	Short: "Get GitHub contributions.",
-	Long:  "Get GitHub contributions.",
-	Run: func(cmd *cobra.Command, args []string) {
-		msg := gngc.GetGitHubContributions()
-		if isNotified {
-			// go run main.go -n (IFTTT に通知を送信する。)
-			gngc.NotifyIFTTT(msg)
-		} else {
-			// go run main.go
-			fmt.Println(msg)
+	Use:           "gngc",
+	Short:         "Get and Notify GitHub Contributions",
+	Long:          "A simple command line application to get GitHub contributions and notify them to IFTTT.",
+	SilenceErrors: true,
+	// SilenceUsage:  true,
+
+	RunE: runCommand,
+}
+
+func runCommand(cmd *cobra.Command, args []string) error {
+	if config.GitHub.UserName == "" && config.GitHub.Token == "" {
+		return errors.New("Credentials for GitHub dont properly configured in $HOME/.gngc.toml.")
+	}
+
+	msg, err := gngc.GetGitHubContributions(config.GitHub)
+	if err != nil {
+		return err
+	}
+
+	// go run main.go -n
+	// IFTTT にメッセージを送信した結果のレスポンスのメッセージが作成される。
+	if isNotified {
+		if config.IFTTT.EventName == "" && config.IFTTT.Token == "" {
+			return errors.New("Credentials for IFTTT dont properly configured in $HOME/.gngc.toml.")
 		}
-	},
+
+		msg, err = gngc.NotifyIFTTT(config.IFTTT, msg)
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Println(msg)
+
+	return nil
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error:", err)
+			os.Exit(1)
+		}
+
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".gngc")
+		viper.SetConfigType("toml")
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(1)
+	}
+
+	if err := viper.Unmarshal(&config); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(1)
+	}
 }
 
 func init() {
-	rootCmd.PersistentFlags().BoolVarP(&isNotified, "notify", "n", false, "Get GitHub contributions and notify them to IFTTT.")
+	cobra.OnInitialize(initConfig)
 
-	gngc.LoadEnv()
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gngc.toml)")
+	rootCmd.PersistentFlags().BoolVarP(&isNotified, "notify", "n", false, "Get GitHub contributions and notify them to IFTTT.")
 }
 
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	cobra.CheckErr(rootCmd.Execute())
 }
